cf0c832e6b18e07696601c9c49595e196dcafdc2
Svapnil Rathod 16 <144311070+Svapnil16@users.noreply.github.com>
2024-06-17 10:48:57 +0530

    Create 0633_Sum_of_Square_Numbers.java

---
+++ June_2024/0633_Sum_of_Square_Numbers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long a = 0 , b = (long) Math.sqrt(c);
+        while ( a <= b )
+        {
+            long s = a * a + b * b;
+            if ( s == c) 
+            {
+                return true;
+            }
+            else if ( s < c )
+            {
+                a++;
+            }
+            else 
+            b--;
+        }
+        return false;
+    }
+}
