41ce09ba760b38afc0d49ac62cd473eb14cd5a9c
Svapnil Rathod 16 <144311070+Svapnil16@users.noreply.github.com>
2024-06-25 10:26:56 +0530

    Create 1038_Binary_Search_Tree_to_Greater_Sum_Tree.java

---
+++ June_2024/1038_Binary_Search_Tree_to_Greater_Sum_Tree.java
@@ -0,0 +1,28 @@
+class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+        int s = 0;
+        TreeNode node = root;
+        while (root != null) {
+            if (root.right == null) {
+                s += root.val;
+                root.val = s;
+                root = root.left;
+            } else {
+                TreeNode next = root.right;
+                while (next.left != null && next.left != root) {
+                    next = next.left;
+                }
+                if (next.left == null) {
+                    next.left = root;
+                    root = root.right;
+                } else {
+                    s += root.val;
+                    root.val = s;
+                    next.left = null;
+                    root = root.left;
+                }
+            }
+        }
+        return node;
+    }
+}
