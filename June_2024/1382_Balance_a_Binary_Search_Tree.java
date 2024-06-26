9587f38dd1c79d905520a937dda31c8373d26780
Svapnil Rathod 16 <144311070+Svapnil16@users.noreply.github.com>
2024-06-26 16:04:46 +0530

    Create 1382_Balance_a_Binary_Search_Tree.java

---
+++ June_2024/1382_Balance_a_Binary_Search_Tree.java
@@ -0,0 +1,27 @@
+class Solution {
+    private List<Integer> nums = new ArrayList<>();
+
+    public TreeNode balanceBST(TreeNode root) {
+        dfs(root);
+        return build(0, nums.size() - 1);
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        nums.add(root.val);
+        dfs(root.right);
+    }
+
+    private TreeNode build(int i, int j) {
+        if (i > j) {
+            return null;
+        }
+        int mid = (i + j) >> 1;
+        TreeNode left = build(i, mid - 1);
+        TreeNode right = build(mid + 1, j);
+        return new TreeNode(nums.get(mid), left, right);
+    }
+}
