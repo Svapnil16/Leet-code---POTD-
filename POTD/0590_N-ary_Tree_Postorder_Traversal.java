b55bca89154ab437d6d8a567cc0480b1b8f25a14
Svapnil Rathod <144311070+Svapnil16@users.noreply.github.com>
2024-08-26 23:05:09 +0530

    Create 0590_N-ary_Tree_Postorder_Traversal.java

---
+++ POTD/0590_N-ary_Tree_Postorder_Traversal.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> postorder(Node root) {
+        LinkedList<Integer> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<Node> stk = new ArrayDeque<>();
+        stk.offer(root);
+        while (!stk.isEmpty()) {
+            root = stk.pollLast();
+            ans.addFirst(root.val);
+            for (Node child : root.children) {
+                stk.offer(child);
+            }
+        }
+        return ans;
+    }
+}
