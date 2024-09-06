cd7e265ce34a67477f6a2e45358bff190c8561c4
Svapnil Rathod <144311070+Svapnil16@users.noreply.github.com>
2024-09-06 13:40:49 +0530

    Create 3217_Delete_Nodes_From_Linked_List_Present_in_Array.java

---
+++ POTD/3217_Delete_Nodes_From_Linked_List_Present_in_Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> s = new HashSet<>();
+        for (int x : nums) {
+            s.add(x);
+        }
+        ListNode dummy = new ListNode(0, head);
+        for (ListNode pre = dummy; pre.next != null;) {
+            if (s.contains(pre.next.val)) {
+                pre.next = pre.next.next;
+            } else {
+                pre = pre.next;
+            }
+        }
+        return dummy.next;
+    }
+}
