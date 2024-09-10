b328602e3595cd5651b359f0c5d6e6dd8e7c46a3
Svapnil Rathod <144311070+Svapnil16@users.noreply.github.com>
2024-09-10 23:20:30 +0530

    Create 2807_Insert_Greatest_Common_Divisors_in_Linked_List.java

---
+++ POTD/2807_Insert_Greatest_Common_Divisors_in_Linked_List.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        for (ListNode pre = head, cur = head.next; cur != null; cur = cur.next) {
+            int x = gcd(pre.val, cur.val);
+            pre.next = new ListNode(x, cur);
+            pre = cur;
+        }
+        return head;
+    }
+
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
