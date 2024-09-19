0f1362b0d7ae06a33fdcae915af845465335e413
Svapnil Rathod <144311070+Svapnil16@users.noreply.github.com>
2024-09-19 21:34:31 +0530

    Create 0241_Different_Ways_to_Add_Parentheses.java

---
+++ POTD/0241_Different_Ways_to_Add_Parentheses.java
@@ -0,0 +1,52 @@
+public class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> result = new ArrayList<>();
+
+        if (input == null || input.length() == 0) {
+            return result;
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (!isOperator(c)) {
+                continue;
+            }
+            List<Integer> left = diffWaysToCompute(input.substring(0, i));
+            List<Integer> right = diffWaysToCompute(input.substring(i + 1));
+            for (int num1 : left) {
+                for (int num2 : right) {
+                    int val = calculate(num1, num2, c);
+                    result.add(val);
+                }
+            }
+        }
+
+            // only contains one number
+        if (result.isEmpty()) {
+            result.add(Integer.parseInt(input));
+        }
+
+        return result;
+    }
+
+        private int calculate(int num1, int num2, char operator) {
+            int result = 0;
+
+            switch(operator) {
+                case '+' : result = num1 + num2;
+                    break;
+
+                case '-' : result = num1 - num2;
+                    break;
+
+                case '*' : result = num1 * num2;
+                    break;
+            }
+
+            return result;
+        }
+
+        private boolean isOperator(char operator) {
+            return (operator == '+') || (operator == '-') || (operator == '*');
+        }
+    }
